2af54622550319e6cf2928b16ef6eb68c3b6c986
LagomaxK <lagomaxravenna+kart@gmail.com>
2025-09-03 12:10:49 +0200

    Update orari.js

--- orari.js
+++ orari.js
@@ -1,5 +1,3 @@
-lun = 'In caso di maltempo o pista bagnata la pista rimarrà chiusa';
-mar = 'In case of bad weather or wet track will remain closed';
 lun = 'Chiuso';
 mar = '15:00 - 20:00';
 mer = '15:00 - 20:00';
@@ -7,6 +5,7 @@ gio = '09:30 - 12:00 | 15:00 - 22:00';
 ven = '09:30 - 12:00 | 15:00 - 22:00';
 sab = '09:30 - 12:00 | 15:00 - 22:00';
 dom = '09:30 - 12:00 | 15:00 - 22:00';
+
 
 
 
