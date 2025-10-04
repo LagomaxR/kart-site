83235c261522b9656fbe47586b7b3b7b7049d126
LagomaxR <lagomaxravenna+ristorante@gmail.com>
2025-10-04 11:14:50 +0200

    Update orari.js

--- orari.js
+++ orari.js
@@ -1,10 +1,11 @@
 lun = 'Chiuso';
-mar = '15:00 - 19:00';
-mer = '15:00 - 19:00';
-gio = '15:00 - 22:00';
-ven = '15:00 - 22:00';
-sab = '09:30 - 12:00 | 15:00 - 22:00';
-dom = '09:30 - 12:00 | 15:00 - 21:00';
+mar = '15:00 - su prenotazione (minimo 4 persone) fino al tramonto';
+mer = '15:00 - su prenotazione (minimo 4 persone) fino al tramonto';
+gio = '15:00 - al tramonto';
+ven = '15:00 - al tramonto';
+sab = '09:30 - 12:00 | 15:00 - al tramonto';
+dom = '09:30 - 12:00 | 15:00 - al tramonto';
+
 
 
 
